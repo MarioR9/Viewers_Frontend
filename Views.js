73bda0c95835fb88e284fed5231467ac94a89228
MarioR9 <mariorodriguezan@gmail.com>
2019-11-06 15:20:20 -0500

    Proxy server connection to website

--- Views.js
+++ Views.js
@@ -8,49 +8,22 @@ let numOfViewers = 0;
 
 
 (async () => {
-  const browser = await puppeteer.launch({headless:false});
-  const page = await browser.newPage();
+   
+//////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////// 1 proxy ///////////////////////////////////////
 
-  await page.goto('Proxy here')
-  await page.type("#maintextfield","website to views here.")
-  await page.keyboard.press('Enter')
+const oldProxyUrl = 'http://PROXY_SERVER:PORT';
+const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
 
-  await page.waitForSelector("[data-a-target= 'browse-channels-button']")
+const browser = await puppeteer.launch({
+  headless:false,
+  args: [`--proxy-server=${newProxyUrl}`],
+});
 
-  await page.click("[data-a-target= 'browse-channels-button']")
+const page = await browser.newPage();
+console.log("connecting to " + newProxyUrl)
+await page.goto(site,{waitUntil: 'networkidle0'})
 
-  const inputElement = await page.$('input[type=search]');
-  
-  await inputElement.click();
-
-
-  await page.screenshot({path: 'ROUTE FOR SCREEN SHOT HERE'});
-
-  await page.waitFor(2000)
-
-  await browser.close();
-  
+await browser.close();
 
 })();
-
-
-
-
-  
-  // console.log('Rainfall Submitted!');
-  // await page.waitFor(2000)
-  ///////////////////////////////////////////////////////////////////////////////////
-  //automated Account Creation 
-  // await page.click("[data-a-target = 'signup-button']")
-  // await page.waitFor(2000)
-  // await page.type("[data-a-target = 'tw-input']", "kevinLamping",{delay: 60})
-  // await page.type("#password-input", 'KevinLamping',{delay: 60})
-  // await page.click("[data-a-target = 'birthday-month-select']");
-  // await page.type("select[data-a-target = 'birthday-month-select']", "January");
-  // await page.click("[data-a-target = 'birthday-date-input']")
-  // await page.type("[data-a-target = 'birthday-date-input']", '23',{delay: 60})
-  // await page.click("[data-a-target = 'birthday-year-input']")
-  // await page.type("[data-a-target = 'birthday-year-input']", '1991',{delay: 60})
-  // await page.click("[data-a-target = 'signup-email-input']")
-  // await page.type("[data-a-target = 'signup-email-input']", 'test@gmail.com',{delay: 60})
-  ///////////////////////////////////////////////////////////////////////////////////
