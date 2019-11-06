058aecc07df7ca20ee60d365486251045ac4939e
MarioR9 <mariorodriguezan@gmail.com>
2019-11-06 15:25:19 -0500

    3rd proxy browser test

--- Views.js
+++ Views.js
@@ -13,33 +13,51 @@ let numOfViewers = 0;
 //////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// 1 proxy ///////////////////////////////////////
 
-const oldProxyUrl = 'http://PROXY_ADDRESS:PORT';
-const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
+  const oldProxyUrl = 'http://PROXY_ADDRESS:PORT';
+  const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
 
-const browser = await puppeteer.launch({
-  headless:false,
-  args: [`--proxy-server=${newProxyUrl}`],
+  const browser = await puppeteer.launch({
+    headless:false,
+    args: [`--proxy-server=${newProxyUrl}`],
 });
 
-const page = await browser.newPage();
-console.log("connecting to " + newProxyUrl)
-await page.goto(site,{waitUntil: 'networkidle0'})
+  const page = await browser.newPage();
+  console.log("connecting to " + newProxyUrl)
+  await page.goto(site,{waitUntil: 'networkidle0'})
 
 // // //////////////////////////////////////////////////////////////////////////////////////////
 // // ////////////////////////////////// 2 proxy //////////////////////////////////////////////
-
-const oldProxyUrl2 = 'http://PROXY_ADDRESS:PORT';
-const newProxyUrl2 = await proxyChain.anonymizeProxy(oldProxyUrl2);
-
-const browser2 = await puppeteer.launch({
-  headless:false,
-  args: [`--proxy-server=${newProxyUrl2}`],
+  
+  const oldProxyUrl2 = 'http://PROXY_ADDRESS:PORT';
+  const newProxyUrl2 = await proxyChain.anonymizeProxy(oldProxyUrl2);
+ 
+  const browser2 = await puppeteer.launch({
+    headless:false,
+    args: [`--proxy-server=${newProxyUrl2}`],
 });
 
-const page2 = await browser2.newPage();
-console.log("connecting to " + newProxyUrl2)
-await page2.goto(site,{waitUntil: 'networkidle0'})
+  const page2 = await browser2.newPage();
+  console.log("connecting to " + newProxyUrl2)
+  await page2.goto(site,{waitUntil: 'networkidle0'})
+  
+// // //////////////////////////////////////////////////////////////////////////////////////////
+// // ////////////////////////////////// 3  proxy /////////////////////////////////////////////
 
-await browser.close();
-await browser2.close();
+  const oldProxyUrl3 = 'http://PROXY_ADDRESS:PORT';
+  const newProxyUrl3 = await proxyChain.anonymizeProxy(oldProxyUrl3);
+
+  const browser3 = await puppeteer.launch({
+    headless:false,
+    args: [`--proxy-server=${newProxyUrl3}`],
+});
+
+  const page3 = await browser3.newPage();
+  console.log("connecting to " + newProxyUrl3)
+  await page3.goto(site,{waitUntil: 'networkidle0'})
+
+// // //////////////////////////////////////////////////////////////////////////////////////////
+// // /////////////////////////////////////////////////////////////////////////////////////////
+  await browser.close();
+  await browser2.close();
+  await browser3.close();
 })();
