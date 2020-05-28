7ca98e87a87717377be27ec99c0d73b4a17ec92b
marior9 <mariorodriguezan@gmail.com>
2020-05-28 13:00:59 -0400

    navigation removed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Views from './Components/Views'
-import NavBar from './Components/Navigation'
 import './App.css';
 
 function App() {
   return (
   <div>
           <div className="App">
-          <div>
-          <NavBar/>
-          </div>
           <div className="Center-Container">
           <Views/>
           </div>
