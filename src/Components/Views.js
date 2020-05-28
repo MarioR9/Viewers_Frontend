e76a5433ee38a06c86d6b9b7e303e1d63338c778
marior9 <mariorodriguezan@gmail.com>
2020-05-28 01:50:58 -0400

    minor changes to fetch request

--- src/Components/Views.js
+++ src/Components/Views.js
@@ -7,13 +7,14 @@ export default class Viewers extends React.Component{
     super(props);
     this.state = {channel: '',
                   numOfViewers: 0,
-                  time: 0,
-                  status: "notActive"
+                  status: true,
+                  
                   }
   }
 //Fetching to backend.. response not working yet..
   handleRequest=()=>{
-    const data = {website: "this.state.channel", numOfViewers: "this.state.numOfViewers", time: "this.state.time"}
+    this.setState({status: false})
+    const data = {website: this.state.channel, numOfViewers: this.state.numOfViewers, status: "active"}
     console.log(data)
     const options = {
       method: 'POST',
@@ -22,7 +23,7 @@ export default class Viewers extends React.Component{
        },
       body: JSON.stringify(data),
     }
-    fetch(`https://us-central1-viewers-c9c49.cloudfunctions.net/app/api`, options)
+    fetch(`https://viewersbackend.herokuapp.com/api`, options)
     .then(resp=>resp.json())
     .then(data => {console.log(data)})
 
@@ -32,7 +33,26 @@ export default class Viewers extends React.Component{
     });
 
   }
+  handleStatusState=()=>{
+    this.setState({status: false})
+    const data = {website: this.state.channel, numOfViewers: this.state.numOfViewers, status: "close"}
+    console.log(data)
+    const options = {
+      method: 'POST',
+      headers : { 
+        'Content-Type': 'application/json',
+       },
+      body: JSON.stringify(data),
+    }
+    fetch(`https://viewersbackend.herokuapp.com/api`, options)
+    .then(resp=>resp.json())
+    .then(data => {console.log(data)})
 
+    .catch(err => {
+      // Error handling
+      console.log("Error Reading data " + err);
+    });
+  }
   
   
   render(){
@@ -40,8 +60,6 @@ export default class Viewers extends React.Component{
       
       <div className="Form-Container">
         <div>{this.state.status}</div>
-      <Form>
-        <Form.Group >
           {/* CHannel or webiste not determine yet. Could have a drop down menu instead with main streaming platforms */}
           <Form.Label>Enter Channel Website</Form.Label>
           <Form.Control onChange={(e)=>{this.setState({ channel: e.currentTarget.value })}} type="text" placeholder="https://www.twitch.tv/user"/>
@@ -50,16 +68,11 @@ export default class Viewers extends React.Component{
           </Form.Text>
           <Form.Label>Desired Number of Viewers</Form.Label>
           <Form.Control onChange={(e)=>{this.setState({ numOfViewers: e.currentTarget.value })}} type="text" placeholder="Number of Viewers" />
-          <Form.Label>Time</Form.Label>
-          <Form.Control onChange={(e)=>{this.setState({ time: e.currentTarget.value })}} type="text" placeholder="time" />
-        </Form.Group>
-        
-        <Button onClick={this.handleRequest} variant="primary" type="submit">
-        Send
-         
-        </Button>
-      </Form>
-        
+          {this.state.status === false ?
+          <Button onClick={this.handleStatusState} variant="danger" type="submit">Stop</Button>
+          :
+          <Button onClick={this.handleRequest} variant="success" type="submit"> Send</Button>
+          }
       </div>
     )
   }
